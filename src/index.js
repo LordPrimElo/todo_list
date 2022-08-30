64d24e6cbc92b9d2f84f4499a615d6a56434813e
LordPrimElo <evansrathore@gmail.com>
2022-08-30 17:06:14 +0530

    Add basic code for project creation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
-import "./index.css"
+// DOM Element Declarations
+const projectsList = document.getElementById("projects-list")
+const addBtn = document.getElementById("add-todo")
+const addForm = document.getElementById("add-form")
+const form = document.getElementById("add-form-main")
 
+// Constants
+const projects = new Array
+
+// Project Factory Function
+const Project = (name) => {
+    const _name = name
+
+    const todos = []
+
+    const addTodo = (toDo) => {
+        todos.push(toDo)
+    }
+
+    const removeTodo = (toDo) => {
+        todos.splice(todos.indexOf(toDo), 1)
+    }
+
+    return { addTodo, removeTodo, _name }
+}
+
+// Helper Functions
+
+const displayProjects = () => {
+    projects.forEach(project => {
+
+        const projectElement = document.createElement("div")
+        projectElement.innerText = project._name
+
+        projectElement.classList.add("project")
+        projectsList.appendChild(projectElement)
+    })
+}
+
+const addProject = (project) => {
+    projects.push(project)
+    displayProjects()
+}
+
+
+addBtn.onclick = () => {
+    if (addForm.classList.contains("hidden")) {
+        addForm.classList.remove("hidden")
+
+    } else {
+        addForm.classList.add("hidden")
+
+        console.log("fdhh")
+    }
+}
